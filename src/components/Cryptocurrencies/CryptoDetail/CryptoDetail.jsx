32646aab0913b8d12f31af657bb8400cda618f47
Dima <lost-is-died@mail.ru>
2022-02-13 18:51:08 +0500

    added crypto Detail

--- src/components/Cryptocurrencies/CryptoDetail/CryptoDetail.jsx
+++ src/components/Cryptocurrencies/CryptoDetail/CryptoDetail.jsx
@@ -1,7 +1,147 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useGetCryptoDetailsQuery} from "../../../services/CryptoApi";
+import {useParams} from "react-router-dom";
+import {
+    DollarCircleOutlined,
+    ThunderboltOutlined,
+    TrophyOutlined,
+    FundOutlined,
+    MoneyCollectOutlined,
+    StopOutlined,
+    ExclamationCircleOutlined,
+    NumberOutlined, CheckOutlined
+} from "@ant-design/icons";
+import millify from "millify";
+import s from './CryptoDetail.module.css'
+import HTMLReactParser from "html-react-parser";
 
+const CryptoDetail = () => {
+    const {cryptoId} = useParams()
+    const {data, isFetching} = useGetCryptoDetailsQuery(cryptoId)
+    const cryptoDetails = data?.data?.coin;
+    // console.log(cryptoDetails)
+    const times = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y']
+    const stats = [
+        {
+            title: "Price to USD",
+            value: `$ ${cryptoDetails && millify(cryptoDetails.price)}`,
+            icon: <DollarCircleOutlined/>
+        },
+        {title: "Rank", value: cryptoDetails && cryptoDetails.rank, icon: <NumberOutlined/>},
+        {
+            title: "24h Volume",
+            value: `$ ${cryptoDetails && millify(cryptoDetails["24hVolume"])}`,
+            icon: <ThunderboltOutlined/>
+        },
+        {
+            title: "Market Cap",
+            value: `$ ${cryptoDetails && millify(cryptoDetails.marketCap)}`,
+            icon: <DollarCircleOutlined/>
+        },
+        {
+            title: "All-time-high(daily avg.)",
+            value: `$ ${cryptoDetails && millify(cryptoDetails.allTimeHigh.price)}`,
+            icon: <TrophyOutlined/>
+        }
+    ]
+    const genericStats = [
+        {title: "Number of Markets", value: cryptoDetails && cryptoDetails.numberOfMarkets, icon: <FundOutlined/>},
+        {
+            title: "Number of Exchanges",
+            value: cryptoDetails && cryptoDetails.numberOfExchanges,
+            icon: <MoneyCollectOutlined/>
+        },
+        {
+            title: "Approved Supply",
+            value: cryptoDetails && cryptoDetails.approvedSupply ? <CheckOutlined/> : <StopOutlined/>,
+            icon: <ExclamationCircleOutlined/>
+        },
+        {
+            title: "Total Supply",
+            value: `$ ${cryptoDetails && millify(cryptoDetails.supply.total)}`,
+            icon: <ExclamationCircleOutlined/>
+        },
+        {
+            title: "Circulating supply",
+            value: `$ ${cryptoDetails && millify(cryptoDetails.supply.circulating)}`,
+            icon: <ExclamationCircleOutlined/>
+        }
+    ]
+    const [time, setTime] = useState()
 
-const CryptoDetail = () =>{
-    return <div>Detail Crypto</div>
+    return (
+        <>
+            {
+                cryptoDetails && (
+                    <>
+                        <div className={s.heading}>
+                            <h1>{cryptoDetails?.name} price</h1>
+                            <p>{cryptoDetails?.name} live price in US dollars. View value statisticks, market cap and
+                                supply</p>
+                            <select placeholder="Select a time period" value={time}
+                                    onChange={(e) => setTime(e.currentTarget.value)}>
+                                <option value="7d">7d</option>
+                                {
+                                    times.map((el, index) => {
+                                        return <option key={index} value={el}>{el}</option>
+                                    })
+                                }
+                            </select>
+                        </div>
+                        <div className={s.statistics}>
+                            <div className={s.statistics_content}>
+                                <div className={s.statistics}>
+                                    <div className={s.statistics_headline}>
+                                        <h1>{cryptoDetails.name} value statistic</h1>
+                                        <p>An overview showing the stats of {cryptoDetails.name}</p>
+                                    </div>
+                                    {stats?.map(({icon, title, value}) => {
+                                        return <div className={s.statistics_item}>
+                                            <div className={s.icons_and_text}>
+                                                <span className={s.icon}>{icon}</span>
+                                                <span className={s.title}>{title}</span>
+                                            </div>
+                                            <div className={s.value_of_statistic}>{value}</div>
+                                        </div>
+                                    })}
+                                </div>
+                                <div className={s.statistics}>
+                                    <div className={s.statistics_headline}>
+                                        <h1>Other statistic</h1>
+                                        <p>An overview showing the stats of {cryptoDetails.name}</p>
+                                    </div>
+                                    {genericStats?.map(({icon, title, value}) => {
+                                        return <div className={s.statistics_item}>
+                                            <div className={s.icons_and_text}>
+                                                <span className={s.icon}>{icon}</span>
+                                                <span className={s.title}>{title}</span>
+                                            </div>
+                                            <div className={s.value_of_statistic}>{value}</div>
+                                        </div>
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                        <div className={s.coin_description}>
+                            <h1>What is {cryptoDetails.name}?</h1>
+                            {HTMLReactParser(cryptoDetails.description)}
+                            <div className={s.links}>
+                                <h1>{cryptoDetails.name} links</h1>
+                                {cryptoDetails.links.map( link => {
+                                    return (
+                                        <div className={s.coin_link}>
+                                            <span>{link.type}</span>
+                                            <a href={link.url}>{link.name}</a>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </>
+
+                )
+            }
+        </>
+    )
 }
 export default CryptoDetail;
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,8 +2,9 @@ import React, {useState} from 'react';
 import s from './New.module.css'
 import {useGetCryptosNewsQuery} from "../../services/CryptoNewsApi";
 import moment from "moment";
-import { Select } from 'antd';
+import {Select} from 'antd';
 import {useGetCryptosQuery} from "../../services/CryptoApi";
+import {NavLink} from "react-router-dom";
 
 const {Option} = Select
 
@@ -17,49 +18,53 @@ const News = ({symplifyed}) => {
         count: symplifyed ? 6 : 12
     })
     if (isFetching) return "Loading..."
-
+    console.log(cryptoNews)
     return (
         <>
             {!symplifyed && (
-                <select placeholder="Select a Cripto" value={newsCategory} onChange={(e) => setNewsCategory(e.currentTarget.value)}>
+                <select placeholder="Select a Cripto" value={newsCategory}
+                        onChange={(e) => setNewsCategory(e.currentTarget.value)}>
                     <option value="Cryptocurrency">Cryptocurrency</option>
                     {
-                        data?.data?.coins.map( (el,index) =>{
-                            return <option key = {index} value={el.name}>{el.name}</option>
-                        } )
+                        data?.data?.coins.map((el, index) => {
+                            return <option key={index} value={el.name}>{el.name}</option>
+                        })
                     }
                 </select>
             )}
             <div className={s.grid_items_news}>
                 {
                     cryptoNews?.value?.map((el, index) => {
-                        return <div className={s.news_item} key={index}>
-                            <div className={s.news_item_title}>
-                                <div className={s.news_title_text}>
-                                    <h3>{el.name}</h3>
-                                </div>
-                                <div className={s.news_title_image}>
-                                    <img src={el?.image?.thumbnail?.contentUrl} alt=""/>
-                                </div>
-                            </div>
-                            <div className={s.description_news}>
-                                <p>{el.description > 100
-                                    ? `${el.description.substring(0, 100)}...`
-                                    : el.description
-                                }</p>
-                            </div>
-                            <div className={s.provider_news}>
-                                <div className={s.provider_and_time}>
-                                    <div className={s.provider_name_img}>
-                                        <img
-                                            src={el.provider[0]?.image?.thumbnail?.contentUrl ? el.provider[0]?.image?.thumbnail?.contentUrl : baseImage}
-                                            alt=""/>
-                                        <span>{el?.provider[0]?.name}</span>
+                        return (
+                            <a href ={el.url}>
+                                <div className={s.news_item} key={index}>
+                                    <div className={s.news_item_title}>
+                                        <div className={s.news_title_text}>
+                                            <h3>{el.name}</h3>
+                                        </div>
+                                        <div className={s.news_title_image}>
+                                            <img src={el?.image?.thumbnail?.contentUrl} alt=""/>
+                                        </div>
                                     </div>
-                                    <span>{moment(el.datePublished).startOf('ss').fromNow()}</span>
+                                    <div className={s.description_news}>
+                                        <p>{el.description > 100
+                                            ? `${el.description.substring(0, 100)}...`
+                                            : el.description
+                                        }</p>
+                                    </div>
+                                    <div className={s.provider_news}>
+                                        <div className={s.provider_and_time}>
+                                            <div className={s.provider_name_img}>
+                                                <img
+                                                    src={el.provider[0]?.image?.thumbnail?.contentUrl ? el.provider[0]?.image?.thumbnail?.contentUrl : baseImage}
+                                                    alt=""/>
+                                                <span>{el?.provider[0]?.name}</span>
+                                            </div>
+                                            <span>{moment(el.datePublished).startOf('ss').fromNow()}</span>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+                            </a>)
                     })
                 }
 
